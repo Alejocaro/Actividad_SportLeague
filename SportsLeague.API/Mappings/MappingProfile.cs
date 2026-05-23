8370080403bacc9b45f14640dded789624ee35f4
Alejocaro <Alejandroc818@gmail.com>
2026-05-22 19:53:38 -0500

    project implementation phase 5

--- SportsLeague.API/Mappings/MappingProfile.cs
+++ SportsLeague.API/Mappings/MappingProfile.cs
@@ -60,6 +60,26 @@ namespace SportsLeague.API.Mappings
                     dest => dest.RefereeFullName,
                     opt => opt.MapFrom(src =>
                         src.Referee.FirstName + " " + src.Referee.LastName));
+
+            // MatchResult mappings
+            CreateMap<MatchResultRequestDTO, MatchResult>();
+            CreateMap<MatchResult, MatchResultResponseDTO>();
+
+            // Goal mappings
+            CreateMap<GoalRequestDTO, Goal>();
+            CreateMap<Goal, GoalResponseDTO>()
+                .ForMember(
+                    dest => dest.PlayerName,
+                    opt => opt.MapFrom(src =>
+                        src.Player.FirstName + " " + src.Player.LastName));
+
+            // Card mappings
+            CreateMap<CardRequestDTO, Card>();
+            CreateMap<Card, CardResponseDTO>()
+                .ForMember(
+                    dest => dest.PlayerName,
+                    opt => opt.MapFrom(src =>
+                        src.Player.FirstName + " " + src.Player.LastName));
         }
     }
 }
