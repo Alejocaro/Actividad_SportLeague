905f2afc2266459571774529b4076cfe505e1827
Alejocaro <Alejandroc818@gmail.com>
2026-05-22 19:43:38 -0500

    Implementacion fase 4

--- SportsLeague.Domain/Entities/Team.cs
+++ SportsLeague.Domain/Entities/Team.cs
@@ -11,5 +11,7 @@
         // Navigation Property - Colección de jugadores
         public ICollection<Player> Players { get; set; } = new List<Player>();
         public ICollection<TournamentTeam> TournamentTeams { get; set; } = new List<TournamentTeam>();
+        public ICollection<Match> HomeMatches { get; set; } = new List<Match>();
+        public ICollection<Match> AwayMatches { get; set; } = new List<Match>();
     }
 }
